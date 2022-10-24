876035fc9033a1f4307701e773a667f6da85e032
gerdefarias <115645802+gerdefarias@users.noreply.github.com>
2022-10-24 14:32:52 -0300

    atualizacao layout

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ root.render(
     <h1>Calculadora</h1>
     <Calculator />
 
+    <h4>By Gerde Farias</h4>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
